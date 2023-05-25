752161eef72e34e793b722b83e0274d2fa444bae
Dhruv Patel <dhruv225@outlook.com>
2023-05-25 19:55:26 -0400

    Add more data to populate

--- populatedb.js
+++ populatedb.js
@@ -26,6 +26,81 @@ const carsData = [
     distance: 5000,
     condition: "Used",
   },
+  {
+    make: "Honda",
+    model: "Accord",
+    year: 2022,
+    price: 27000,
+    distance: 8000,
+    condition: "New",
+  },
+  {
+    make: "Ford",
+    model: "Mustang",
+    year: 2020,
+    price: 40000,
+    distance: 5000,
+    condition: "Used",
+  },
+ 
+  {
+    make: "Chevrolet",
+    model: "Camaro",
+    year: 2021,
+    price: 35000,
+    distance: 12000,
+    condition: "Used",
+  },
+  {
+    make: "Nissan",
+    model: "Altima",
+    year: 2019,
+    price: 20000,
+    distance: 15000,
+    condition: "Used",
+  },
+  {
+    make: "BMW",
+    model: "X5",
+    year: 2021,
+    price: 60000,
+    distance: 10000,
+    condition: "New",
+  },
+  {
+    make: "Audi",
+    model: "A4",
+    year: 2020,
+    price: 35000,
+    distance: 8000,
+    condition: "Used",
+  },
+  {
+    make: "Mercedes-Benz",
+    model: "C-Class",
+    year: 2021,
+    price: 50000,
+    distance: 6000,
+    condition: "New",
+  },
+  {
+    make: "Hyundai",
+    model: "Elantra",
+    year: 2018,
+    price: 15000,
+    distance: 20000,
+    condition: "Used",
+  },
+  {
+    make: "Kia",
+    model: "Sorento",
+    year: 2021,
+    price: 30000,
+    distance: 12000,
+    condition: "New",
+  },
+
+
   // Add more sample car data here
 ];
 
