19a59674043d00658f02e6a70dcc15348d166abf
Dhruv Patel <dhruv225@outlook.com>
2023-05-25 17:12:28 -0400

    Install express-async-handler

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// /cars
+// /cars/id
+// /cars/create
+// /cars/id/update
+// /cars/id/delete
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
