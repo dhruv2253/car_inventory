bf22d069a41a9f9285907e1fc73a42e1d1046d50
Dhruv Patel <dhruv225@outlook.com>
2023-05-26 20:44:58 -0400

    Handle post request

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,6 +1,6 @@
 const Car = require("../models/car");
 const asyncHandler = require("express-async-handler");
-
+const { body, validationResult } = require("express-validator");
 // Get car home page
 exports.index = asyncHandler(async(req, res, next) => {
     // Get details of cars
@@ -29,14 +29,43 @@ exports.car_detail = asyncHandler(async(req, res, next) => {
 
 // Display car create form on GET.
 exports.car_create_get = asyncHandler(async(req, res, next) => {
-    res.send("NOT IMPLEMENTED: Car create GET");
+    res.render("car_form", {
+        title: "Create Car"
+    });
 });
 
 // Handle car create on POST.
-exports.car_create_post = asyncHandler(async(req, res, next) => {
-    res.send("NOT IMPLEMENTED: Car create POST");
-});
+exports.car_create_post = [
+    body("make", "Make must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("model", "Model must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("year", "Year must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("price", "Price must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("distance", "Distance must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("condition", "Condition must not be empty.").trim().isLength({ min: 1 }).escape(),
 
+    asyncHandler(async(req, res, next) => {
+    const errors = validationResult(req);
+    const car = new Car({
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        price: req.body.price,
+        distance: req.body.distance,
+        condition: req.body.condition
+    })
+    if (!errors.isEmpty()) {
+        res.render("car_form", {
+            title: "Create Car",
+            car: car,
+            errors: errors.array()
+        });
+        return;
+    } else {
+        await car.save();
+        res.redirect(car.url);
+    }
+}),
+]
 // Display car delete form on GET.
 exports.car_delete_get = asyncHandler(async(req, res, next) => {
     res.send("NOT IMPLEMENTED: Car delete GET");
